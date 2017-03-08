f74270c55665f2d6bff44d8f0e0b5d4fdeeaa0e8
Lucas Siqueira <lucassouza.ti@gmail.com>
2017-03-07 23:03:02 -0300

    implementado paralelizacao, mas apresentado problemas em vetores maiores ou iguais a 10, e pulando alguns numeros na distribuicao

---
+++ bolha_normal.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h> 
+// #include <conio.h>
+ 
+int main(void)
+{
+    float *v; //definindo o ponteiro v
+    int i, j, aux, num_componentes;    
+     
+    printf("Informe o numero de componentes do vetor\n");
+    scanf("%d", &num_componentes);
+    
+      
+    v = (float *) malloc(num_componentes * sizeof(float));  
+    
+    //Armazenando os dados em um vetor
+    for (i = 0; i < num_componentes; i++)
+    {
+      printf("\nDigite o valor para a posicao %d do vetor: ", i+1);
+      scanf("%f",&v[i]);
+    }
+    
+    for( i = 0; i < num_componentes; i++ )
+    {
+    for( j = i + 1; j < num_componentes; j++ ) // sempre 1 elemento à frente
+    {
+      // se o (i > (i+1)) então o i passa pra frente (ordem crescente)
+      if ( v[i] > v[j] )
+      {
+         aux = v[i];
+         v[i] = v[j];
+         v[j] = aux;
+      }
+    }
+  }  
+     
+   // ------ Percorrendo o vetor e imprimindo os valores ----------
+   printf("\n*********** Valores do vetor dinamico ************\n\n");
+    
+   for (i = 0;i < num_componentes; i++)
+   {
+       printf("%.2f\n",v[i]);
+   }
+   
+          
+    
+   //liberando o espaço de memória alocado 
+   free(v);
+    
+   // getch();
+   return 0;
+} 
